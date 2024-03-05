ad0e04f927bcbff4a0db7e2a7082e4eb13021cb1
PoliAna19 <anastasiya.paliachok@gmail.com>
2024-03-05 19:45:50 +0400

    second task solved

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,18 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const merged = {};
+  objects.forEach((obj) =>
+    Object.entries(obj).forEach(([i, j]) => {
+      if (i in merged) {
+        merged[i] += j;
+      } else {
+        merged[i] = j;
+      }
+    })
+  );
+  return merged;
 }
 
 /**
